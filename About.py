4f1a322ef6d05f6784a9e4587a9d1c67df76bd65
tinesite3333 <152348843+tinesite3333@users.noreply.github.com>
2023-12-06 10:31:20 +0800

    Add files via upload

--- About.py
+++ About.py
@@ -22,9 +22,10 @@ with st.container():
 with image_column:
     st.image(img_contact_form)
 
-with st.container():
+with text_column:
+
     st.title("About My Family")
-    st.write("This is my family picture way back when I graduated at Senior High School")
+    st.write("This picture was taken way back when I graduated from Senior High School.")
     st.write("I'm the youngest among two sibling and I have a half sister")
     st.write(
         "My family motivates me to become successful, they are my reason  why I keep pushing everyday.")
@@ -44,7 +45,70 @@ with st.container():
 with image_column:
     st.image(img_contact_form)
 
-with st.container():
+with text_column:
+
     st.title("My older brother")
     st.write("He motivates me to strive harder in life.His been an incredible source of strength and I'm endlessly grateful for everything he do.")
     st.write("His resilience,determination and the way he handles challenges head-on inspire me everyday.")
+
+import streamlit as st
+  
+    # --- LOAD ASSETS ---
+lottie_coding = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20 fcfjwiyb.json")
+img_contact_form = Image.open ("images about/hakdog.jpg")
+st.title("My Friends")
+# --- PROJECTS ---
+with st.container():
+    image_column, text_column = st.columns ((1, 2))
+with image_column:
+    st.image(img_contact_form)    
+
+import streamlit as st
+  
+    # --- LOAD ASSETS ---
+lottie_coding = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20 fcfjwiyb.json")
+img_contact_form = Image.open ("images about/highschool.jpg")
+# --- PROJECTS ---
+with st.container():
+    image_column, text_column = st.columns ((1, 2))
+with image_column:
+    st.image(img_contact_form)    
+
+    import streamlit as st
+  
+    # --- LOAD ASSETS ---
+lottie_coding = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20 fcfjwiyb.json")
+img_contact_form = Image.open ("images about/classmates (2).jpg")
+# --- PROJECTS ---
+with st.container():
+    image_column, text_column = st.columns ((1, 2))
+with image_column:
+    st.image(img_contact_form)    
+
+ # --- LOAD ASSETS ---
+lottie_coding = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20 fcfjwiyb.json")
+img_contact_form = Image.open ("images about/HAHAHA.jpg")
+# --- PROJECTS ---
+with st.container():
+    image_column, text_column = st.columns ((1, 2))
+with image_column:
+    st.image(img_contact_form)    
+
+with st.container():
+    st.write("They are my escape from reality. When we are together I feel free and I am who I am when I'm with them and  I am valued, loved, cared, and I dont feel left out at all. They are my source of happiness and I can instantly forget my problem when Im hanging out with them.")
+    st.write("They help me grow. I will treasure them and the memories that we had toegther.")
+
+import streamlit as st
+  
+    # --- LOAD ASSETS ---
+lottie_coding = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20 fcfjwiyb.json")
+img_contact_form = Image.open ("images about/sister.jpg")
+# --- PROJECTS ---
+with st.container():
+    image_column, text_column = st.columns ((1, 2))
+with image_column:
+    st.image(img_contact_form)    
+
+with text_column:
+    st.title("Ate Mai")
+    st.write("She is my half-sister. I can easily open up with her and share my thoughts. She is there to listen and give advice without judgement. She let me explore that things I never tried.")
